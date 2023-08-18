c87048dcad6bdef6b9089572dff3a138c69dcf40
RenuSuresh <rganvir500@gmail.com>
2023-08-18 17:50:41 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Sidebar from "../components/Sidebar";
+import { Fragment } from "react";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Spotify</title>
+        <Partytown debug={true} forward={["dataLayer.push"]} />
+      </Head>
+      <div className="bg-black h-screen overflow-hidden">
+        <main>
+          <Sidebar />
+          {/* center */}
+        </main>
+        <div>{/* player */}</div>
+      </div>
+    </Fragment>
+  );
+}
