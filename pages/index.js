f73b81df1f741c10629ec0c14e33a8695886b7ee
RenuSuresh <34194473+RenuSuresh@users.noreply.github.com>
2023-08-25 17:37:55 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,42 @@ export default function Home() {
     <Fragment>
       <Head>
         <title>Spotify</title>
-        <Partytown debug={true} forward={["dataLayer.push"]} />
+        import { Html, Head, Main, NextScript } from 'next/document'
+import { Partytown } from '@builder.io/partytown/react';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head >
+
+        <Partytown
+          debug={true}
+          forward={['dataLayer.push']}
+        />
+
+        <script
+          defer
+          type='text/partytown'
+          dangerouslySetInnerHTML={{
+            __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5CMB6RLC');
+      `,
+          }}
+        />
+
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
       </Head>
       <div className="bg-black h-screen overflow-hidden">
         <main>
